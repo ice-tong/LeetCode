6c041acdbc0689b9222ed1e9311c36ef9559875f
ice-tong <32220263+ice-tong@users.noreply.github.com>
2018-07-12 17:17:29 +0800

    Update singleNumber.cpp

--- Array/singleNumber.cpp
+++ Array/singleNumber.cpp
@@ -12,13 +12,3 @@ public:
 		return tmp;
 	}
 };
-int main() {
-	vector<int> nums;
-	nums.push_back(4);
-	nums.push_back(1);
-	nums.push_back(2);
-	nums.push_back(1);
-	nums.push_back(2);
-	cout << Solution().singleNumber(nums) << endl;
-	return 0;
-}
